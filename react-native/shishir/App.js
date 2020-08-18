6e9f331a49b237f2c841f2f2083511173516bf09
Restyled.io <commits@restyled.io>
2020-08-18 22:49:21 +0000

    Restyled by clang-format

--- react-native/shishir/App.js
+++ react-native/shishir/App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
